6bfff5e91b268aa0cc6cfa3c837e636a556bab8f
Marion Vieillard <marion.vieillard@teamwork.net>
2026-01-19 20:45:25 +0100

    Chat depuis navigateur fonctionnel - style CSS OK

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,10 @@
+import express from "express";
+import http from "http";
+import { ChatSocket } from "./chat/chat.socket.js";
+import { WhoIAmControler } from "./whoami/whoami.controler.js";
+import { ChatControler } from "./chat/chat.controler.js";
+import cors from "cors";
+
 const PORT = 3000;
 const app = express();
 const server = http.createServer(app);
@@ -7,7 +14,8 @@ const server = http.createServer(app);
 ChatSocket.INSTANCE.setup(server);
 
 // Front+Api
-app.use(express.static("..front/dist"));
+// app.use(express.static("../frontend/dist"));
+app.use(cors());
 app.use(express.json());
 app.get("/api/whoami", WhoIAmControler.get);
 app.post("/api/chat", ChatControler.post);
---
+++ frontend/src/main.ts
@@ -0,0 +1,56 @@
+import { io } from "socket.io-client";
+import "./style.css";
+
+const form = document.querySelector("form")!;
+const input = form.querySelector("input[name='message']")! as HTMLInputElement;
+
+const CHANNEL = "message";
+
+function connectWs(ip: string) {
+  const socket = io(`http://${ip}:3000`);
+  socket.on("connect", () => console.log("Connecté au serveur WebSocket"));
+  socket.on(CHANNEL, (msg) => displayMessage(msg));
+}
+
+function displayWhoami(serverIp: string) {
+  const div = document.getElementById("whoami")!;
+  div.textContent = serverIp;
+}
+
+function displayMessage(message: string) {
+  const div = document.getElementById("message")!;
+  const paragraph = div.appendChild(document.createElement("p"));
+  paragraph.classList.add("msg-paragraph");
+  paragraph.textContent = message;
+  window.scrollTo({
+    top: document.body.scrollHeight,
+    behavior: "smooth",
+  });
+}
+
+window.addEventListener("load", async () => {
+  const call = await fetch("/api/whoami");
+  const json = await call.json();
+  displayWhoami(json.ip);
+  connectWs(json.ip);
+  input.focus();
+});
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const message = input.value;
+  if (!message) return;
+
+  await fetch("/api/chat", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      message: message,
+    }),
+  });
+
+  input.value = "";
+});
